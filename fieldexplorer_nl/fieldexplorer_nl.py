ff1d686ccfcc2053dc3ed2e10c4d16580be147d9
Richard Duivenvoorde <richard@duif.net>
2020-07-01 14:43:34 +0200

    Small addition of info to the info dialog

--- fieldexplorer_nl/fieldexplorer_nl.py
+++ fieldexplorer_nl/fieldexplorer_nl.py
@@ -45,6 +45,8 @@ class FieldExplorerNl(object):
         # connect the action to the work/run method
         self.action.triggered.connect(self.run)
         help_txt = """FieldExplorer Information
+
+Version number: 1.1
         
 This application is used to generate plot files for the application in the FieldExplorer.
 
@@ -53,6 +55,8 @@ Please note:
 - Ensure no plots are touching or overlapping. 
 - Plots are required to be defined with 4 corners. 
 - Please ensure the attributes "Plot-ID" and "Comments" are used for identification of the plots.
+- A Plot-ID cannot contain any of the following characters:   \ / : * ?  < > |
+- Maximum length of a Plot-ID is 50 characters
 
 For questions or comments to this plugin, please contact us via info@phenokey.com
 
